259d57ccab44d763d6085d9dcc6801c154fd064d
Alexander Avakov <yaavakov@gmail.com>
2018-03-03 16:38:05 +0300

    Create default achievement types inside migration

--- database/migrations/1520079130290_achievement_type_schema.js
+++ database/migrations/1520079130290_achievement_type_schema.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const moment = require('moment')
+
+const Database = use('Database')
 const Schema = use('Schema')
 
 class AchievementTypeSchema extends Schema {
@@ -9,6 +12,19 @@ class AchievementTypeSchema extends Schema {
       table.string('name')
       table.string('code')
       table.timestamps()
+    })
+
+    this.schedule(async (trx) => {
+      const now = moment().format('YYYY-MM-DD HH:mm:ss')
+      const rows = [
+        { name: 'Лайк ВК', code: 'vk_like' },
+        { name: 'Репост ВК', code: 'vk_repost' },
+        { name: 'Другое', code: 'other' }
+      ].map(row => ({ ...row, created_at: now, updated_at: now }))
+
+      await Database.table('achievement_types')
+        .transacting(trx)
+        .insert(rows)
     })
   }
 
