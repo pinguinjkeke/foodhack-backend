8c12fca05be1ee6f4b6762dd2020930af519ccfc
Alexander Avakov <yaavakov@gmail.com>
2018-03-03 20:43:27 +0300

    Register slugifyProvider in the right place

--- start/app.js
+++ start/app.js
@@ -18,7 +18,8 @@ const providers = [
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/shield/providers/ShieldProvider',
   '@adonisjs/session/providers/SessionProvider',
-  '@adonisjs/auth/providers/AuthProvider'
+  '@adonisjs/auth/providers/AuthProvider',
+  '@adonisjs/lucid-slugify/providers/SlugifyProvider'
 ]
 
 /*
@@ -32,7 +33,6 @@ const providers = [
 */
 const aceProviders = [
   '@adonisjs/lucid/providers/MigrationsProvider',
-  '@adonisjs/lucid-slugify/providers/SlugifyProvider'
 ]
 
 /*
